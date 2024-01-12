b4f3e599291c81847277612c45aad06a348fb8d4
Akshit Chilkoti <86488967+Akshit-Chilkoti@users.noreply.github.com>
2024-01-12 08:39:29 +0000

    add contact form , slide show and footer(only in idex, incomplete)

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ window.onscroll = function() {
         icon.style.display = "none";
     }
 };
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
